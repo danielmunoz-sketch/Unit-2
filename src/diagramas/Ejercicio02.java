bec2cc9451e0d7f25ae5ffa4c02050968f503df3
daniel.munoz <daniel.munoz@106-17.info2>
2025-10-14 13:28:42 +0200

    ejercicios 1-5

---
+++ src/diagramas/Ejercicio02.java
@@ -0,0 +1,10 @@
+package diagramas;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/diagramas/Ejercicio05.java
@@ -0,0 +1,22 @@
+package diagramas;
+
+import java.util.Scanner;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner (System.in);
+		
+		int num;
+		System.out.println("Introduzca un número: ");
+		num = reader.nextInt();
+		
+		if (num>=0) {
+			System.out.println("Es positivo");
+		}else {
+			System.out.println("Es negativo");
+		}
+		reader.close();
+	}
+
+}
