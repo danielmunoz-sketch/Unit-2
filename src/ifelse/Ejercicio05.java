c01ba1648699dd0355511bf2f7a1340072497b72
danielmunoz-sketch <daniel.munoz@iesnervion.es>
2025-10-19 13:08:33 +0200

    Update Ejercicio05.java

--- src/ifelse/Ejercicio05.java
+++ src/ifelse/Ejercicio05.java
@@ -8,21 +8,21 @@ public class Ejercicio05 {
 		Scanner reader = new Scanner (System.in);
 		
 		String jug1;
-    String jug2;
+    	String jug2;
     
 		System.out.println("Introduzca las jugadas: ");
 		jug1 = reader.next();
-    jug2 = reader.next();
+   	 	jug2 = reader.next();
 		
-    if(jug1.equals(jug2)) {
-      System.out.println("Empate");
-    } else if(jug1.equals(Piedra) && jug2.equals(Tijeras) || jug1.equals(Papel) && jug2.equals(Piedra) || jug1.equals(Tijeras) && jug2.equals(Papel)) {
-      System.out.println("Gana el jugador 1");
-	  }else {
-      System.out.println("Gana el jugador 2");
-    }
+    	if(jug1.equals(jug2)) {
+      		System.out.println("Empate");
+    	} else if(jug1.equals(Piedra) && jug2.equals(Tijeras) || jug1.equals(Papel) && jug2.equals(Piedra) || jug1.equals(Tijeras) && jug2.equals(Papel)) {
+      		System.out.println("Gana el jugador 1");
+		}else {
+      		System.out.println("Gana el jugador 2");
+    	}
 
-    reader.close();
-  }
+    	reader.close();
+  	}
 
 }
