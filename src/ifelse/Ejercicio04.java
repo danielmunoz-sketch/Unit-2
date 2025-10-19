bad2ceb1f87e4a94d1e2208a184721aa34bf737a
danielmunoz-sketch <daniel.munoz@iesnervion.es>
2025-10-19 12:44:49 +0200

    Create Ejercicio04.java

---
+++ src/ifelse/Ejercicio04.java
@@ -0,0 +1,31 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner (System.in);
+		
+		int num;
+		System.out.println("Introduzca un número: ");
+		num = reader.nextInt(); 
+		
+    if(num < 0 || num > 99999) {
+      System.out.println("Número incorrecto. Introduzca un número entre 0 y 99999");
+    } else if(number < 10) {
+      System.out.println("Tiene una cifra.");
+		}else if(number < 100) {
+      System.out.println("Tiene dos cifras.");
+    }else if(number < 1000) {
+      System.out.println("Tiene tres cifras.");
+    }else if(number < 10000) {
+      System.out.println("Tiene cuatro cifras.");
+    }else if(number < 100000) {
+      System.out.println("Tiene cinco cifras.");
+    }
+    
+		reader.close();
+	}
+
+}
