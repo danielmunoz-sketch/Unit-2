6109d270366e0fef2a5ad5c059daa39e7949cb8e
danielmunoz-sketch <daniel.munoz@iesnervion.es>
2025-10-19 13:09:32 +0200

    Update Ejercicio04.java

--- src/ifelse/Ejercicio04.java
+++ src/ifelse/Ejercicio04.java
@@ -11,21 +11,21 @@ public class Ejercicio04 {
 		System.out.println("Introduzca un número: ");
 		num = reader.nextInt(); 
 		
-    if(num < 0 || num > 99999) {
-      System.out.println("Número incorrecto. Introduzca un número entre 0 y 99999");
-    } else if(number < 10) {
-      System.out.println("Tiene una cifra.");
-	}else if(number < 100) {
-      System.out.println("Tiene dos cifras.");
-    }else if(number < 1000) {
-      System.out.println("Tiene tres cifras.");
-    }else if(number < 10000) {
-      System.out.println("Tiene cuatro cifras.");
-    }else if(number < 100000) {
-      System.out.println("Tiene cinco cifras.");
-    }
+    	if(num < 0 || num > 99999) {
+      		System.out.println("Número incorrecto. Introduzca un número entre 0 y 99999");
+    	} else if(number < 10) {
+      		System.out.println("Tiene una cifra.");
+		}else if(number < 100) {
+     		System.out.println("Tiene dos cifras.");
+    	}else if(number < 1000) {
+      		System.out.println("Tiene tres cifras.");
+    	}else if(number < 10000) {
+      		System.out.println("Tiene cuatro cifras.");
+    	}else if(number < 100000) {
+      		System.out.println("Tiene cinco cifras.");
+    	}
     
-	reader.close();
+		reader.close();
 	}
 
 }
